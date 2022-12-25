c49615a79b720b51fa0f2a83a345584e76d65cbf
mahmoud-hassan45 <116374410+mahmoud-hassan45@users.noreply.github.com>
2022-12-25 16:49:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Transactions;
+
+int i,j;
+Console.WriteLine("enter the first number ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("enter the max");
+int max = int.Parse(Console.ReadLine ());
+for (i = a; i <= max; i++)
+{
+    int count = 0;
+    for (j = 1; j <= i; j++)
+    {
+
+        if (i % j == 0)
+        { count = count + 1; }
+    }
+
+    if (count <= 2)
+    {
+        Console.WriteLine( i ); 
+    }
+
+
+}
+
+
+
+
+
+
+
